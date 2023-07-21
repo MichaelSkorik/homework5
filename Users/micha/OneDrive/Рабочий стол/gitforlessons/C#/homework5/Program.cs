4418f126803c7f27388357634228b11fe74241fa
Michael <Michael@SKorik.com>
2023-07-21 14:09:51 +0300

    lkok

--- Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework5/Program.cs
+++ Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework5/Program.cs
@@ -2,17 +2,3 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 
 
-Console.Clear();
-int arraySize = new Random().Next(2,8);
-int[] array = new int[arraySize];
-int evenCount = 0;
-for (int i = 0; i < arraySize; i++)
-{
-    array[i] = new Random().Next(100, 1000);
-    if (array[i] % 2 == 0)
-    {
-        evenCount++;
-    }
-}
-var str = string.Join(" ", array);
-Console.WriteLine($"Количество четных чисел в массиве [{str}]  --> " + evenCount);
