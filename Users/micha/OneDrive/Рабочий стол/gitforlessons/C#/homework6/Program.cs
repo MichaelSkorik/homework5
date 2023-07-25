d8ed9a8ff8581b3b7a46196b37b86a9330b4cde0
Michael <Michael@SKorik.com>
2023-07-25 10:54:19 +0300

    Ex_41

--- Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework6/Program.cs
+++ Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework6/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Clear();
+int[] numbers;
+int ans = 0;
+int num = new Random().Next(2, 11);
+Console.WriteLine($"Enter {num} numbers");
+numbers = new int[num];
+for (int i = 0; i < num; i++)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    if (numbers[i] > 0)
+    {
+        ans++;
+    }
+}
+Console.WriteLine(ans);
