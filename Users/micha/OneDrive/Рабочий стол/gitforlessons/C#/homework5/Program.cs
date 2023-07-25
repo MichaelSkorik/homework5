b27145bdf4c571a151b7a8336691e176519185f7
Michael <Michael@SKorik.com>
2023-07-25 10:18:18 +0300

    Ex_36

--- Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework5/Program.cs
+++ Users/micha/OneDrive/Рабочий стол/gitforlessons/C#/homework5/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Clear();
+int arraySize = new Random().Next(2,8);
+int[] array = new int[arraySize];
+int size = 0;
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if(i % 2 == 1)
+    {
+        size = size + array[i];
+    }
+}
+var str = string.Join(" ", array);
+Console.WriteLine($"сумма чисел  стоящих на нечетных позициях в массиве [{str}]  --> " + size);
